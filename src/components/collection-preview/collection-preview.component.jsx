715184e9a3f5664f24f4eb86cc0d410e8ba4d103
wesleywil <wesleywilsonti@gmail.com>
2022-04-29 18:23:08 -0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flox for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -12,7 +12,8 @@ const CollectionPreview = ({title, items}) => (
                 .filter((item, idx) => idx < 4)
                 .map((item) => (
                     <CollectionItem key={ item.id } item={item}/>
-            ))}
+                ))
+            }
             
         </div>
     </div>
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,12 +8,21 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import './collection.styles.scss';
-<CollectionItem/>
+
 const CollectionPage = ({ collection }) => {
-    console.log('MY CONSOLE' + JSON.stringify(collection));
+    const { title, items } = collection;
     return (
-        <div className="collection">
-            <h2> COLLECTION PAGE</h2>
+        <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+                {
+                    items.map(item => (
+                    <CollectionItem key={item.id} item={item}/>
+
+                    ))
+
+                }
+            </div>
         </div>
     );
 };
